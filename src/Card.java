67fd6b024f8b60eac20eaa53141c972033957bab
deanroberts <deano.roberts@menloschool.org>
2024-11-19 14:59:04 -0800

    Finished card class

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+public class Card
+{
+    private String rank;
+    private String suit;
+    private int points;
 
-public class Card {
+    public Card(String rank, String suit, int points) {
+        this.rank = rank;
+        this.suit = suit;
+        this.points = points;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
 }
